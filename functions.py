787c91505d393ce53f958bbd51d2593de5d42b0c
Sebastian Seguin <sebastianmseguin@gmail.com>
2026-02-01 19:54:06 -0500

    added chest function

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
 import random
+import lootTables
+import loot.current as current
 
 def loot(lootTable, lootExtraSpace, luck, luckModifier):
     return lootTable[((random.randint(0, (len(lootTable) - lootExtraSpace))) + (luck * luckModifier))]
+
+def chest():
+    return loot(lootTables.chestLoot.lootTable, lootTables.chestLoot.extraSpace, current.luck, lootTables.chestLoot.luckModifier)
